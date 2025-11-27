11ce860d1d11c1bf4bb52972202871824c490360
Samuel Rodríguez <samuelrodriguezrey@outlook.com>
2025-11-27 16:11:20 +0100

    add notify slack custom action

---
+++ actions/notify-slack/main.js
@@ -0,0 +1,22 @@
+import * as core from "@actions/core";
+
+const message = core.getInput("message", { required: true });
+const webhookUrl = core.getInput("webhook-url", { required: true });
+
+// const message = process.env.INPUT_MESSAGE
+
+(async () => {
+  const res = await fetch(webhookUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ test: message }),
+  });
+
+  if (!res.ok) {
+    console.error(`Slack API responded with ${res.status}`);
+    process.exit(1);
+  }
+
+  console.log("Message sent succesfully ✅");
+  process.exit(0);
+})();
